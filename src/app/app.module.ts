a966d9541768eba9450008a310f3a9ea751c2896
Troy Center <troycenter1@gmail.com>
2017-07-18 10:33:30 -0700

    finished assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HeaderComponent } from '../../src/app/header/header.component';
+import { NavComponent } from '../../src/app/nav/nav.component';
+import { MainComponent } from '../../src/app/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+		HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
